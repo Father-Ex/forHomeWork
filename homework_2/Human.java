b250a95536b354f11c2eaac21aa7a5aadd776533
Father-Ex <49256068+Father-Ex@users.noreply.github.com>
2022-01-10 02:44:41 +0900

    Add files via upload

--- homework_2/Human.java
+++ homework_2/Human.java
@@ -2,6 +2,25 @@ package homework_2;
 
 public class Human implements Runnable,Speak {
 
+    private int speed;
+    private String direction;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
     @Override
     public void loud() {
         System.out.println("오버라이딩 된 loud메소드");
